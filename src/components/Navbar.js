cfc840591e58fee4ba263684ae2497d15e13f0fb
Emilly Natacha <emilly123taurino@gmail.com>
2022-12-26 09:36:25 -0300

    estilo do botão

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaBookOpen } from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 function Navbar() {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click) 
   return(
     <>
-    <div className='navbar'>
-            <h1>OOOOIII</h1>
-    </div>
+      <div className='navbar'>
+          <div className='navbar-container container'>
+            <Link to='/' className='navbar-logo'>
+              <FaBookOpen className='navbar-icon' />
+              LIVRESE
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars /> }
+            </div>   
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links'>
+                  Home
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/services' className='nav-links'>
+                  Services
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/products' className='nav-links'>
+                  Products
+                </Link>
+              </li>
+              <li className='nav-btn'>
+                  {button ? (
+                    <Link to='/sign-up' className='btn-link'>
+                      <Button buttonStyle='btn--outline'>Sign up</Button>  
+                    </Link>
+                  ) : (
+                    <Link to='/sign-up' className='btn-link'>
+                      <Button buttonStyle='btn--outline'
+                      buttonSize='btn--mobile'>
+                      
+                      Sign up</Button>
+                    </Link>
+                  )}
+              </li>
+            </ul> 
+          </div>
+      </div>
     </> 
   )
 }
