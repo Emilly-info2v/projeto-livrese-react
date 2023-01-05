3c31362e1e21ae270ee552e0951204364a8f1edf
Emilly Natacha <emilly123taurino@gmail.com>
2023-01-04 23:10:00 -0300

    atualização da tela login

--- src/components/pages/Cadastro.js
+++ src/components/pages/Cadastro.js
@@ -1,8 +1,65 @@
 import React from 'react'
+import { FaBookOpen } from 'react-icons/fa'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Cadastro.css'
 
 function Cadastro() {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [name,setName] = useState("")
+
   return (
-    <div><h1>Cadastro</h1></div>
+    <div className='container-2'>
+      <div className='container-login'>
+        <div className='wrap-login'>
+          <form className='login-form'>
+            <span className='title-login'>Faça o seu cadastro no Livre - se</span>
+            <span className='title-login'><FaBookOpen className='login-icon' /></span>
+
+            <div className='wrap-input'>
+              <input 
+                className={name !== "" ? 'has-val input' : 'input'} 
+                type= "text"
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+              <span className='focus-input' data-placeholder='Nome completo'></span>
+            </div>
+
+            <div className='wrap-input'>
+              <input 
+                className={email !== "" ? 'has-val input' : 'input'} 
+                type= "text"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+              <span className='focus-input' data-placeholder='Email'></span>
+            </div>
+
+            <div className='wrap-input'>
+              <input 
+              className={password !== "" ? 'has-val input' : 'input'} 
+              type= "password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              />
+              <span className='focus-input' data-placeholder='Passaword'></span>
+            </div>
+            
+            <div className='container-login-form-btn'>
+              <button className='login-form-btn'>Cadastrar</button>
+            </div>
+
+            <div className='text-center'>
+              <span className='txt-1'>Já tenho uma conta? </span>
+              <Link to='/login'className='txt-2'>Faça login </Link>
+            </div>
+
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,54 +1,69 @@
 import React from 'react'
-import { FaBookOpen } from 'react-icons/fa'
+import { MdEmail, MdLock } from 'react-icons/md'
+import { HiEyeOff, HiEye } from "react-icons/hi";
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
+import Logo from'./../imgs/logo-livrese.jpeg'
 
 function Login() {
   const [email,setEmail] = useState("")
   const [password,setPassword] = useState("")
+  const [show,setShow] = useState(false)
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    setShow(!show)
+  }
 
   return (
-    <div className='container-2'>
-      <div className='container-login'>
-        <div className='wrap-login'>
-          <form className='login-form'>
-            <span className='title-login'>Entre com seu login</span>
-            <span className='title-login'><FaBookOpen className='login-icon' /></span>
+    <> 
+      <div className='login'>
+        <div className='img-logo'>
+          <img src={Logo} alt="Livre -se"/>
+        </div>
 
-            <div className='wrap-input'>
-              <input 
-                className={email !== "" ? 'has-val input' : 'input'} 
-                type= "text"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
+          <div className='login-right'>
+                    <h2>Entre com o seu login</h2>
+            <div className='loginInputEmail'>
+              <MdEmail />
+              <input
+              type='text'
+              placeholder='Digite o seu e-mail'
+              valeu={email}
+              onChange={e => setEmail(e.target.value)}
               />
-              <span className='focus-input' data-placeholder='Email'></span>
             </div>
-
-            <div className='wrap-input'>
-              <input 
-              className={password !== "" ? 'has-val input' : 'input'} 
-              type= "password"
-              value={password}
+            
+            <div className='loginInputPassword'>
+              <MdLock/>
+              <input
+              type={show ? 'text' : 'password'}
+              placeholder='Digite a sua senha'
+              valeu={password}
               onChange={e => setPassword(e.target.value)}
               />
-              <span className='focus-input' data-placeholder='Passaword'></span>
+              <div className='login-eye'>
+                {show ? (
+                  <HiEye onClick={handleClick}/>
+                ) : (
+                  <HiEyeOff onClick={handleClick}/>
+                )}
+              </div>
             </div>
 
-            <div className='container-login-form-btn'>
-              <button className='login-form-btn'>Login</button>
-            </div>
+            <button type='submit'>
+              Entrar
+            </button>
 
             <div className='text-center'>
-              <span className='txt-1'>Não possui conta?</span>
-              <Link to='/cadastro'className='txt-2'>Criar conta</Link>
+              <span className='txt-1'>Não tenho uma conta? </span>
+              <Link to='/login'className='txt-2'>Criar conta </Link>
             </div>
 
-          </form>
-        </div>
+          </div>
       </div>
-    </div>
+    </>
   )
 }
 
